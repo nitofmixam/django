cebf29c93ecda4598a2a994435dfbc9e9ee9a55b
Maksim Fotin <maksimfotin144@gmail.com>
2024-07-27 18:35:21 +0700

    add homework 22.2

--- users/forms.py
+++ users/forms.py
@@ -13,14 +13,15 @@ class UserLoginForm(AuthenticationForm):
 
 class RegistrationForm(UserCreationForm):
     class Meta:
-        model = get_user_model()
-        fields = ('email', 'password1', 'password2', 'username', 'avatar', 'phone', 'country', )
-        widgets = {
-            'email': forms.EmailInput(attrs={'class': 'form-input'}),
-            'password1': forms.PasswordInput(attrs={'class': 'form-input'}),
-            'password2': forms.PasswordInput(attrs={'class': 'form-input'}),
-            'username': forms.TextInput(attrs={'class':'form-input'}),
-            'avatar': forms.FileInput(attrs={'class': 'form-input'}),
-            'phone': forms.TextInput(attrs={'class': 'form-input'}),
-            'country': forms.TextInput(attrs={'class': 'form-input'}),
-        }
+        class Meta:
+            model = get_user_model()
+            fields = ('email', 'password1', 'password2', 'username', 'avatar', 'phone', 'country',)
+            widgets = {
+                'email': forms.EmailInput(attrs={'class': 'form-input'}),
+                'password1': forms.PasswordInput(attrs={'class': 'form-input'}),
+                'password2': forms.PasswordInput(attrs={'class': 'form-input'}),
+                'username': forms.TextInput(attrs={'class': 'form-input'}),
+                'avatar': forms.FileInput(attrs={'class': 'form-input'}),
+                'phone': forms.TextInput(attrs={'class': 'form-input'}),
+                'country': forms.TextInput(attrs={'class': 'form-input'}),
+            }
